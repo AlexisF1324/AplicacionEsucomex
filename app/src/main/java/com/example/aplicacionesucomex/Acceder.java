f55ef3a89d7c8e5e3a3c717bf2478a278700fe3e
alexi <alexi@ALEXIS>
2023-08-30 15:53:42 -0400

    Subida1

---
+++ app/src/main/java/com/example/aplicacionesucomex/Acceder.java
@@ -0,0 +1,13 @@
+package com.example.aplicacionesucomex;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Acceder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
